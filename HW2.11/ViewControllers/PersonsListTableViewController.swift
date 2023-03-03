7218a04f20ded61ef0cf71374153f2f154fb158a
Maksim Gurkov <maksim.gurkov@gmail.com>
2023-03-03 22:08:54 +0300

    delete return

--- HW2.11/ViewControllers/PersonsListTableViewController.swift
+++ HW2.11/ViewControllers/PersonsListTableViewController.swift
@@ -28,7 +28,7 @@ class PersonsListTableViewController: UITableViewController {
     
     // MARK: - Table view data source
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return persons.count
+        persons.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
